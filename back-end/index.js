aecc9e37933142876379e67d0299edf578d14fd3
Tealiosv2 <97336208+Tealiosv2@users.noreply.github.com>
2023-03-04 09:46:13 -0800

    routed to index.html in front-end public

--- back-end/index.js
+++ back-end/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 8888;
+const fs = require("fs");
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(fs.readFileSync("../front-end/public/index.html", "utf8"));
 });
 
 app.listen(port, () => {
