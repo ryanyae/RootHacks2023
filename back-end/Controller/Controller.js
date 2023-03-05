ca110116ef69e8896dad78552d76c2330f417415
ryanyae <ryancyae@gmail.com>
2023-03-04 16:05:18 -0800

    Minor Backend Changes

--- back-end/Controller/Controller.js
+++ back-end/Controller/Controller.js
@@ -1,6 +1,6 @@
 
-const {getCurrentWeather} = require("../Weather/WeatherAPI.js")
-const {calcTrueK} = require("../Plant-Data/test")
+const { getCurrentWeather, getAtmPressure } = require("../Weather/WeatherAPI.js")
+const { calcTrueK } = require("../Plant-Data/test")
 
 
 module.exports = {
@@ -10,9 +10,21 @@ module.exports = {
         // const value = searchKtable("pineapple")
         // console.log(value)
         // res.status(200).json({ title: "hello"})
+        const formData = req.body
 
-        var weatherData = await getCurrentWeather();
+        var weatherData = await getCurrentWeather("Vancouver", 1);
+        var atmPressure = await getAtmPressure("Vancouver");
         var trueK = calcTrueK("Zucchini", 10);
-        console.log(trueK)
+
+        var es = (0.6108 * (Math.pow(Math.E, ((17.27 * weatherData.avgtemp_c) / (weatherData.avgtemp_c + 237.3)))))
+        var delta = (4098 * es) / (Math.pow((weatherData.avgtemp_c + 237.3), 2))
+        var A = (0.0016 * (atmPressure / 10)
+            *
+            ((delta + 0.078) / (delta + 0.089))
+        );
+        var evoNum = (0.0588 * (A) * Math.sqrt(weatherData.avgtemp_c) * ((weatherData.avghumidity / 2) + 2.7))
+        var finalNum = (trueK.kval * evoNum * 1)
+        console.log(finalNum)
+        return(finalNum)
     }
 }
--- back-end/Weather/WeatherAPI.js
+++ back-end/Weather/WeatherAPI.js
@@ -1,11 +1,8 @@
 const axios = require('axios')
+const apiKey = "5d382ab21c274e5780a185652230403"
 
-async function getCurrentWeather() {
-
-
-
-    const apiKey = "5d382ab21c274e5780a185652230403"
-
+async function getCurrentWeather(city, int) {
+    
     // axios({
     //     method:"get",
     //     url:" http://api.weatherapi.com/v1/current.json",
@@ -13,15 +10,13 @@ async function getCurrentWeather() {
     //         key:apiKey
     //     }
     // })
-
     var result = await axios.get('http://api.weatherapi.com/v1/forecast.json', {
         params: {
             key: apiKey,
-            q: "Vancouver",
-            days:1
+            q: city,
+            days:int
         }
     })
-
     return (
         // {
         //     temp: result.data.current.temp_c,
@@ -31,4 +26,16 @@ async function getCurrentWeather() {
     )
 }
 
-module.exports = { getCurrentWeather }
+async function getAtmPressure(city) {
+    var result = await axios.get('http://api.weatherapi.com/v1/current.json', {
+        params: {
+            key: apiKey,
+            q: city,
+        }
+    })
+    return (
+        result.data.current.pressure_mb
+    )
+}
+
+module.exports = { getCurrentWeather, getAtmPressure }
